ddfcbf345b5c8154458a843fe5729b91027ede66
Rodolfo Lyu Shimotsu <rodolfoshimotsu@gmail.com>
2023-04-19 22:43:00 -0300

    Testando código e ajustando a janela

--- dino.py
+++ dino.py
@@ -1,41 +1,28 @@
 import pyautogui
-from time import sleep
 from PIL import ImageGrab, ImageOps
+from numpy import *
 
-replay = (962, 292)
-dino = (661, 293, 703, 338)
-delta = 85
-delta2 = 15
-
+replay = (915, 336)
+dino = (624, 343)
 
 def imagegrab():
-    bbox = (dino[0] + delta, dino[1], dino[2] + delta, dino[3] - delta2)
-    img = ImageGrab.grab(bbox)
-    grayimg = ImageOps.grayscale(img)
-    # grayimg.save("target.jpg")
-    # b = grayimg.getcolors()
-    a = sum(map(sum, grayimg.getcolors()))
+    x, y, a, b = 665, 338, 50, 30
+    box = (x,y,x+a,y+b)
+    img = ImageGrab.grab(box)
+    gray = ImageOps.grayscale(img)
+    a = sum(array(gray.getcolors()))
+    print(a)
     return a
-
-
-def jump():
-    pyautogui.keyDown('space')
-    sleep(0.001)
-    pyautogui.keyUp('space')
-    sleep(0.001)
-
 
 def restart():
     pyautogui.click(replay)
-
 
 def main():
     restart()
     while True:
         imagegrab()
-        if (imagegrab() != 1507):
-            jump()
-
+        if (imagegrab() != 1747):
+            pyautogui.press('space')
 
 if __name__ == "__main__":
     main()
